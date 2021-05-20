046432081216c25a5e28ee36850f112270e4ae09
Mesin Kasir <52827888+mesinkasir@users.noreply.github.com>
2021-05-20 13:47:01 +0700

    Update admin.php

--- admin.php
+++ admin.php
@@ -39,9 +39,8 @@
    <div class="col-md-12">
       <div class="panel">
         <div class="jumbotron text-center">
-           <h1>Thank You! for your support and love.</h1>
-           <p> <strong>OSWA-INV v2</strong> way more better then <strong> v1 </strong>.
-           </br>If you have a question regarding the usage of this applications, please ask on <a href="https://www.facebook.com/oswapp" title="Facebook" target="_blank">Facebook</a> OSWA Fan page.</p>
+        
+          <p>Coming soon....</p>
 
         </div>
       </div>
